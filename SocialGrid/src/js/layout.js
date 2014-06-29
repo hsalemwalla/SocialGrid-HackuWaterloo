88fc49fd0a88aff2c234be03063f8b9cefed521f
hamritsidhu <hamritsidhu_94@hotmail.com>
2014-06-29 00:53:20 -0400

    added linkedin suggested jobs plugin

--- SocialGrid/src/js/layout.js
+++ SocialGrid/src/js/layout.js
@@ -2,3 +2,8 @@
 function collapseIntro() {
     $("#intro_bar").hide();
 }
+
+
+function showLinkedin() {
+    $('#linkedinContent').html(' <script src="//platform.linkedin.com/in.js" type="text/javascript"></script> <script type="IN/JYMBII" data-format="inline"></script>');
+}
